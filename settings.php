0a909d309a451e23ff17b698a576918bdccd621e
Boban Blagojević <boban.blagojevic@tsp.edu.rs>
2025-08-15 13:40:10 +0200

    Refactor settings.php: reorganize and enhance file header comments for clarity and consistency

--- settings.php
+++ settings.php
@@ -1,7 +1,35 @@
 <?php
 // This file is part of Moodle - http://moodle.org/.
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details for the AI for Teachers block.
+ *
+ * @package    block_aipromptgen
+ * @author     Boban Blagojevic
+ * @copyright  2025 AI4Teachers
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 defined('MOODLE_INTERNAL') || die();
+
+// Ensure the settings page exists and is registered in the admin tree so a Settings link appears.
+if (!isset($settings)) {
+    $settings = new admin_settingpage('block_aipromptgen', get_string('pluginname', 'block_aipromptgen'));
+    $ADMIN->add('blocksettings', $settings);
+}
 
 if ($ADMIN->fulltree) {
     // OpenAI API key (password-unmask field).
